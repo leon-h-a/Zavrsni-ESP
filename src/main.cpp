74faccf516d7b9fd467190eca7b523132b2ea84f
Naponoel <lekileo@gmail.com>
2020-11-17 17:33:26 +0100

    Whole system working properly (simple LED on/off over remote server)

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,7 @@
+// todo: umjesto Serial debugging-a treba napraviti WiFi / MQTT debugging
+
+
+
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 #include <PubSubClient.h>
@@ -7,8 +11,7 @@ char pass[] = "83911903";
 #define MQTT_srv "172.105.76.166"
 int MQTT_port = 1883;
 
-// Prototypes
-void callback_fnc(const char* topic, byte* payload, unsigned int length);
+void callback_fnc(const char* topic, const byte* payload, unsigned int length);
 
 WiFiClient WiFiClient;
 PubSubClient client(MQTT_srv, 1883, callback_fnc, WiFiClient);
@@ -19,19 +22,19 @@ __unused void setup()
     Serial.begin(9600);
 
     // WiFi configuration
-    Serial.println("Setting up WiFi");
+//    Serial.println("Setting up WiFi");
     WiFi.begin(ssid, pass);
     while (WiFi.status() != WL_CONNECTED)
     {
         delay(500);
-        Serial.print(".");
+//        Serial.print(".");
     }
-    Serial.println();
-    Serial.print("Connected, my IP address: ");
-    Serial.println(WiFi.localIP());
+//    Serial.println();
+//    Serial.print("Connected, my IP address: ");
+//    Serial.println(WiFi.localIP());
 
     // MQTT configuration
-    Serial.println("setting up MQTT");
+//    Serial.println("setting up MQTT");
     client.setClient(WiFiClient);
     client.setServer(MQTT_srv, MQTT_port);
     client.setCallback(callback_fnc);
@@ -39,18 +42,25 @@ __unused void setup()
         client.connect("TheCli");
         delay(500);
     }
-    Serial.println();
-    Serial.print("Connected to MQTT server @ ");
-    Serial.println(MQTT_srv);
+//    Serial.println();
+//    Serial.print("Connected to MQTT server @ ");
+//    Serial.println(MQTT_srv);
     if (client.subscribe("/sub")) {
-        Serial.println("Subscribed to topic /sub");
+//        Serial.println("Subscribed to topic /sub");
     }
 }
 
-void callback_fnc(const char* topic, byte* payload, unsigned int length){
-    Serial.println("Message received from broker");
-    client.publish("/pub","hello back @ u!");
-    Serial.println("Message sent back");
+void callback_fnc(const char* topic, const byte* payload, unsigned int length){
+//    Serial.println("Message received from broker");
+    if(payload[0] == '1'){
+        Serial.write(0b00000001);
+        client.publish("/conf", "LED On");
+
+    }
+    else if (payload[0] == '0'){
+        Serial.write(0b00000010);
+        client.publish("/conf", "LED Off");
+    }
 }
 
 void loop() {
