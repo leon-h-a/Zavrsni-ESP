f524656ec69f39edeaca1a93208907958d5c4cf8
Naponoel <lekileo@gmail.com>
2020-11-17 15:20:40 +0100

    WiFi & MQTT working properly

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,59 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
 
-byte receivedByte;
+char ssid[] = "TP-Link_2BC6";
+char pass[] = "83911903";
+#define MQTT_srv "172.105.76.166"
+int MQTT_port = 1883;
 
-void setup() {
+// Prototypes
+void callback_fnc(const char* topic, byte* payload, unsigned int length);
+
+WiFiClient WiFiClient;
+PubSubClient client(MQTT_srv, 1883, callback_fnc, WiFiClient);
+
+__unused void setup()
+{
+    delay(5000);
     Serial.begin(9600);
-//    while (!Serial) {
-//        ; // wait for serial port to connect. Needed for Native USB only
-//    }
+
+    // WiFi configuration
+    Serial.println("Setting up WiFi");
+    WiFi.begin(ssid, pass);
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+        Serial.print(".");
+    }
+    Serial.println();
+    Serial.print("Connected, my IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // MQTT configuration
+    Serial.println("setting up MQTT");
+    client.setClient(WiFiClient);
+    client.setServer(MQTT_srv, MQTT_port);
+    client.setCallback(callback_fnc);
+    while (!client.connected()){
+        client.connect("TheCli");
+        delay(500);
+    }
+    Serial.println();
+    Serial.print("Connected to MQTT server @ ");
+    Serial.println(MQTT_srv);
+    if (client.subscribe("/sub")) {
+        Serial.println("Subscribed to topic /sub");
+    }
+}
+
+void callback_fnc(const char* topic, byte* payload, unsigned int length){
+    Serial.println("Message received from broker");
+    client.publish("/pub","hello back @ u!");
+    Serial.println("Message sent back");
 }
 
 void loop() {
-    if (Serial.available() > 0) {
-        receivedByte = Serial.read();
-        Serial.println("Primio sam: ");
-        Serial.print(receivedByte);
-    }
-
-    else{
-        Serial.print("Nista\n");
-    }
-
-    delay(1000);
+    client.loop();
+    delay(10);
 }
